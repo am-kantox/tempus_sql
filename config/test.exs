d6b68075f257949fb4627d5d763077df2c11cd7c
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2023-04-13 14:51:10 +0200

    The whole scaffold is done, more funs needed

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ config :tempus_sql, Tempus.SQL.Repo, database: "tempus_sql_test"
 
 if System.get_env("GITHUB_ACTIONS") do
   config :tempus_sql, Tempus.SQL.Repo,
-    hostname: System.get_env("POSTGRES_HOST", "localhost"),
+    hostname: "localhost",
     username: "postgres",
     password: "postgres"
 end
