dc62f4599caa3c84f0b1a0b8da68991316a7251c
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2023-04-13 14:45:38 +0200

    The whole scaffold is done, more funs needed

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :tempus_sql, Tempus.SQL.Repo,
-  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  hostname: "localhost",
   username: "elixir_dev",
   password: "alchemist",
   pool: Ecto.Adapters.SQL.Sandbox
--- config/test.exs
+++ config/test.exs
@@ -2,4 +2,11 @@ import Config
 
 config :tempus_sql, Tempus.SQL.Repo, database: "tempus_sql_test"
 
+if System.get_env("GITHUB_ACTIONS") do
+  config :tempus_sql, Tempus.SQL.Repo,
+    hostname: System.get_env("POSTGRES_HOST", "localhost"),
+    username: "postgres",
+    password: "postgres"
+end
+
 config :logger, level: :error
